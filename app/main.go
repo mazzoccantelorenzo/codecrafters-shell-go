40a48af0eb61d7ed0d7590d0a10492a8c860c1b4
mazzoccantelorenzo <mazzolore97@gmail.com>
2026-03-17 14:11:59 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	// fmt.Print("$ ")
+	fmt.Print("$ ")
 }
