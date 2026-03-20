085a935655376e6754c969dd124e579e6ee27bdd
mazzoccantelorenzo <mazzolore97@gmail.com>
2026-03-20 02:43:18 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,15 +8,15 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
 	for {
 		fmt.Print("$ ")
 		var commands []string
-		// Captures the user's command in the "command" variable
+		// Captures the user's command in the "command" variable.
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		//I remove the final \n to print in the same line after
 		command = strings.Trim(command, "\n")
 
 		isValid := slices.Contains(commands, command)
