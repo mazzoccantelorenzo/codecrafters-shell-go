0c33be203445aa36e35eee282ea10107d3aff5de
mazzoccantelorenzo <mazzolore97@gmail.com>
2026-03-20 13:54:23 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,12 +10,13 @@ import (
 
 // Commands is the list of all existing and valid commands
 
-var commands = []string{EXIT_COMMAND, ECHO_COMMAND}
+var commands = []string{EXIT_COMMAND, ECHO_COMMAND, TYPE_COMMAND}
 
 // List of commands
 
 var EXIT_COMMAND = "exit"
 var ECHO_COMMAND = "echo"
+var TYPE_COMMAND = "type"
 
 func PrintDollar() {
 	fmt.Print("$ ")
@@ -46,6 +47,13 @@ func getCommandFromInput(input string) string {
 	return strings.Split(input, " ")[0]
 }
 
+func getArgumentFromInput(input string, command string) string {
+	// Argument is the last slice of the entire string
+	// 'echo hello world' --> hello world is argument
+	return strings.Split(input, command+" ")[1]
+
+}
+
 func printCommandNotFound(command string) {
 	fmt.Printf("%s: command not found\n", command)
 }
@@ -70,8 +78,21 @@ func main() {
 			// Command is basically the first element of the user's input -> input[0]
 			// We want to get the rest of the string, excluding command that is 'echo' for example
 			// We split the input and get the last element ("echo ", because we want the string without space at the beginning)
-			textToPrint := strings.Split(input, command+" ")[1]
+			textToPrint := getArgumentFromInput(input, command)
 			fmt.Println(textToPrint)
+
+		}
+
+		if command == TYPE_COMMAND {
+			argument := getArgumentFromInput(input, command)
+
+			// If the argument is a command, then we can print 'is a shell builtin'
+			// For example: echo exit --> exit is the argument and it is indeed an existing command
+			if slices.Contains(commands, argument) {
+				fmt.Println(argument, "is a shell builtin")
+			} else {
+				fmt.Println(argument, ": not found")
+			}
 
 		}
 
